ee62ba034ab9586f2e0f17ca999b069c2fd1a7a8
janvanroestel <jcjvanroestel@gmail.com>
2018-10-23 10:55:18 -0700

    new version of everything

--- LC_utils.py
+++ LC_utils.py
@@ -5,6 +5,21 @@ import matplotlib.pyplot as plt
 
 
 
+def R_Eggleton(q):
+    """ the approximation by Eggleton for the volume radius of a roche lobe.
+    Parameters
+    ----------
+    q : float
+        the mass ratio of the binary
+    Returns
+    -------
+    R : float
+        the approximation to the roche lobe volumetric radius
+    """
+    R = 0.49*q**(2./3)
+    R /= 0.6*q**(2./3) + np.log(1+q**(1./3.))
+    return R
+
 
 def WD_MR(M):
     '''calculate the radius of a zero temperature wd given a mass
@@ -83,7 +98,7 @@ def plotall(allpars,alldata,allmodels,grid='default',fold=False,Nbins=False):
 
 
 
-def plotLC(pars,data,model,grid='default',fold=False,Nbins=False):
+def plotLC(pars,data,model,grid='default',error_scaling=False,fold=False,Nbins=False):
     """ A function to plot a single lightcurve
 
     input
@@ -106,28 +121,43 @@ def plotLC(pars,data,model,grid='default',fold=False,Nbins=False):
 
     offset = np.floor(np.min(data[:,0]))
 
-
     p = pars[0]
     t0 = pars[1]
-    m = model(data[:,0],pars,grid) 
+
+    if error_scaling:
+        m = model(data[:,0],pars[:-1],grid) 
+        lnf = pars[-1]
+        sigma = np.sqrt(data[:,2]**2 + m**2*np.exp(2*lnf))
+    else:
+        m = model(data[:,0],pars,grid)
 
     if fold:
-
         phases = (data[:,0]-t0)/p%1
         p_idx = np.argsort(phases)
-        ax1.errorbar(phases[p_idx],data[p_idx,1],data[p_idx,2],fmt='k.')
-        ax1.plot(phases[p_idx],m[p_idx],zorder=3,c='C3')
-        ax2.errorbar(phases[p_idx],data[p_idx,1]-m[p_idx],data[p_idx,2],fmt='k.')    
+        for n in [0,1]:
+            ax1.errorbar(phases[p_idx]+n,data[p_idx,1],data[p_idx,2],fmt='k.')
+            ax2.errorbar(phases[p_idx]+n,data[p_idx,1]-m[p_idx],data[p_idx,2],fmt='k.')    
+            if error_scaling:
+                ax1.errorbar(phases[p_idx]+n,data[p_idx,1],sigma[p_idx],fmt='k.',elinewidth=0.2)
+                ax2.errorbar(phases[p_idx]+n,data[p_idx,1]-m[p_idx],sigma[p_idx],fmt='k.',elinewidth=0.2)
+            ax1.plot(phases[p_idx]+n,m[p_idx],zorder=3,c='C3')
+
         if Nbins:
             bins = np.linspace(0,1,Nbins+1)
             digitized = np.digitize(phases, bins)
             res_means = np.array([np.average(data[digitized==i,1]-m[digitized==i]) for i in range(1, len(bins))])
-            plt.plot((bins[:-1]+bins[1:])/2,res_means,'bx',zorder=3)
+            for n in [0,1]:
+                plt.plot((bins[:-1]+bins[1:])/2+n,res_means,'bx',zorder=3)
 
     else:
         ax1.errorbar(data[:,0]-offset,data[:,1],data[:,2],fmt='k.')
         ax1.plot(data[:,0]-offset,m,zorder=3,c='C3')
         ax2.errorbar(data[:,0]-offset,data[:,1]-m,data[:,2],fmt='k.')
+
+        if error_scaling:
+            ax1.errorbar(data[:,0]-offset,data[:,1],sigma,fmt='k.',elinewidth=0.2)
+            ax2.errorbar(data[:,0]-offset,data[:,1]-m,sigma,fmt='k.',elinewidth=0.2)   
+
         if Nbins:
             bins = np.linspace(np.min(data[:,0]),np.max(data[:,0]),Nbins+1)
             digitized = np.digitize(data[:,0], bins)
@@ -154,4 +184,79 @@ def plotLC(pars,data,model,grid='default',fold=False,Nbins=False):
     chi2 = np.sum(((data[:,1]-m)/data[:,2])**2)
     plt.suptitle( "Chi2/N: %d/%d" %(chi2,np.size(data[:,1])))
 
+
+
+def make_initial_simplex(x0,scales):
+    """ Given a vector and scales on each parameter, make an initial simplex 
+    position
+
+    input:
+        x0: initial point
+        scale: typical scale of each parameter (log)
+
+    output:
+        array with initial simplex positions
+
+    """
+    N = np.size(x0)
+    initial_simplex = np.outer(x0,np.ones([N+1])).T
+    initial_simplex[1:,:] += np.identity(np.size(x0))*10**scales
+    return initial_simplex
+
+
+
+def lnp(pars,data,model,priors,grid='default',error_scaling=False):
+    """ Calculate the lnp given data,model and priors
+
+    input:
+        pars: the parameters
+        data: the lightcurve data (t,y,dy)
+        model: the modelfunction which takes as input (t,pars,grid)
+        priors: function which takes priors and return prior value
+        grid: grid density parameter
+        lnf: bool, if True, the last parameter in pars is assumed to be lnf
+
+    output:
+        lnp: float
+
+    """
+    
+    ln_priors = priors(pars)
+    if np.isinf(ln_priors):
+        return -np.inf
+
+    if error_scaling:
+        lnf = pars[-1] # last parameter is errorscaling parameter
+        m = model(data[:,0],pars[:-1],grid=grid) # make the model LC
+        inv_sigma2 = 1.0/(data[:,2]**2 + m**2*np.exp(2*lnf)) # add extra Var.
+        lnp = -0.5*(np.sum((data[:,1]-m)**2*inv_sigma2 - np.log(inv_sigma2)))
+
+    else:
+        Z = ((model(data[:,0],pars,grid=grid)-data[:,1])/data[:,2])**2
+        lnp = -0.5*np.sum(Z)
+
+    print(np.size(data[:,0]),-2*lnp,-2*ln_priors)
+
+    out = lnp+ln_priors
+    
+    # check for nan
+    if np.isnan(out):
+        out = -np.inf
+
+    return out
+
+
+
+def remove_trend(LC,pf):
+    """remove a trend from the lightcurve"""
+
+    t = LC[:,0]
+    x0 = np.average(t) # the middle of the LC
+    trend = 1+pf[0]*(t-x0)+pf[1]*(t-x0)**2+pf[2]*(t-x0)**3
+    
+    LC[:,1] = LC[:,1]/trend
+    LC[:,2] = LC[:,2]/trend
+
+    return LC
+    
 
